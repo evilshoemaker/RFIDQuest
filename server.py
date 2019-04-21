15d656c4c8cc934fe04a651c20613371569ea7a1
digios <digios.till@yandex.ru>
2019-04-22 02:37:44 +0700

    Tmp

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from flask import Flask, redirect, url_for, request, jsonify
 from flask import render_template
+import serial
+import threading
+import time
 app = Flask(__name__)
 
+rfidReaderSerial1 = None
+rfidReaderSerial2 = None
 gamers = {}
 
 def reset_game():
@@ -55,6 +60,41 @@ def get_gamer_info():
             )
 
     return jsonify(result);
+
+def rfid_reader(id):
+    pass
+
+def rfid_reader1_receive_thread():
+  while rfidReaderSerial1:
+    if rfidReaderSerial1.inWaiting() > 0:
+      c = rfidReaderSerial1.read(1)
+      print( c )
+    else:
+      time.sleep(0.1)
+
+def start_serial(comPortName1, comPortName2):
+    global rfidReaderSerial1
+    global rfidReaderSerial2
+
+    rfidReaderSerial1 = serial.Serial(
+        port=comPortName1,
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS
+    )
+
+    rfidReaderSerial2 = serial.Serial(
+        port=comPortName2,
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS
+    )
+
+    threading.Thread(target=rfid_reader1_receive_thread).start()
+    #threading.Thread(target=ReceiveThread).start()
+
 
 if __name__ == '__main__':
     reset_game()
