74e64c2d4aa2036fa144f2770ffcb4d80cea7e9e
digios <digios.till@yandex.ru>
2019-04-25 02:48:17 +0700

    Tmp

---
+++ templates/gamer_info.php
@@ -0,0 +1,42 @@
+[
+	{
+		"name": "player1",
+		"hp": 25
+	},
+	{
+		"name": "player2",
+		"hp": 90
+	},
+	{
+		"name": "player3",
+		"hp": 72
+	},
+	{
+		"name": "player4",
+		"hp": 66
+	},
+	{
+		"name": "player5",
+		"hp": 48
+	},
+	{
+		"name": "player6",
+		"hp": 30
+	},
+	{
+		"name": "player7",
+		"hp": 15
+	},
+	{
+		"name": "player8",
+		"hp": 5
+	},
+	{
+		"name": "player9",
+		"hp": 100
+	},
+	{
+		"name": "player10",
+		"hp": 0
+	}	
+]
